a940b0e64c1373050999159cfc80572cdcfd99b6
Danilo Vaccalluzzo <danilo.vaccalluzzo@gmail.com>
2025-05-13 12:22:02 +0200

    build for fe and installer

--- buildinstall.bat
+++ buildinstall.bat
@@ -16,3 +16,18 @@ rmdir /s /q backend\build
 echo 📦 Copying new dist folder to backend\react...
 xcopy /E /I /Y fe\dist backend\react
 
+echo 🚀 Entering backend directory and running pyinstaller...
+cd backend
+if exist crawlytics.db del /f /q crawlytics.db
+echo ▶️ Running pyinstaller .\crawlytics.spec
+pyinstaller .\crawlytics.spec
+
+cd dist
+
+echo 🗜️ Zipping Crawlytics folder...
+powershell Compress-Archive -Path Crawlytics -DestinationPath Crawlytics.zip
+
+rmdir /s /q Crawlytics
+
+echo ✅ Done!
+pause
